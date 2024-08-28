36bfc0fbf6b31d01a7af57c5058b3a57753911c1
dunxuan <dunxuan@dunxuan.xyz>
2024-08-28 08:30:17 +0800

    v1.0 实现配置文件读取、初始化 实现获取cookies 实现计时12点运行 实现基本功能 实现查看已预约成功信息 实现打包构建

---
+++ run.py
@@ -0,0 +1,234 @@
+import datetime
+import os
+import re
+from time import sleep
+import tomllib
+from bs4 import BeautifulSoup
+import tomli_w
+import requests
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def get_config():
+    if not os.path.exists("conf.toml"):
+        conf = {
+            "seat": {"seat_area": None, "seat_id": None},
+            "account": {"username": None, "password": None},
+            "data": {
+                "date": None,
+                "segment": None,
+                "PHPSESSID": None,
+                "access_token": None,
+                "expire": None,
+                "user_name": None,
+                "userid": None,
+            },
+            "init": True,
+        }
+        return conf
+    else:
+        with open("conf.toml", "rb") as f:
+            return tomllib.load(f)
+
+
+def get_seat(seat_area):
+    day = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    url = "https://lxl.sdyu.edu.cn/api.php/spaces_old"
+    params = {
+        "area": f"{seat_area}",
+        "day": day,
+        "startTime": "08:30",
+        "endTime": "22:30",
+    }
+    r = requests.get(url=url, params=params)
+    return r.json()["data"]["list"]
+
+
+def get_segment(seat_area):
+    url = f"https://lxl.sdyu.edu.cn/api.php/v3areadays/{seat_area}"
+    r = requests.get(url=url)
+    return r.json()["data"]["list"][1]["id"]
+
+
+def init_config(conf):
+    # 区域
+    df = pd.read_csv("area.csv")
+    print(df.to_string(index=False))
+    while True:
+        conf["seat"]["seat_area"] = int(input("输入区域id:"))
+        if conf["seat"]["seat_area"] in df["id"].values:
+            break
+        else:
+            print("号不对啊")
+
+    # 座位
+    data = get_seat(conf["seat"]["seat_area"])
+    min_seat = data[0]["no"]
+    max_seat = data[-1]["no"]
+    while True:
+        seat_no = int(input(f"输入座位号 ({min_seat}~{max_seat}):"))
+        if seat_no >= int(min_seat) and seat_no <= int(max_seat):
+            conf["seat"]["seat_id"] = (seat_no - int(min_seat)) + data[0]["id"]
+            break
+        else:
+            print("号不对啊")
+
+    # 账号
+    conf["account"]["username"] = input("请输入用户名:")
+    conf["account"]["password"] = input("请输入密码:")
+
+    conf = get_cookies(conf)
+
+    conf["init"] = False
+    with open("conf.toml", "wb") as f:
+        tomli_w.dump(conf, f)
+    return conf
+
+
+def get_cookies(conf, force=False):
+    if conf["data"]["date"] == datetime.date.today() and force is False:
+        return conf
+
+    options = webdriver.EdgeOptions()
+    options.add_argument("--start-maximized")
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Edge(options=options)
+    driver.get(
+        "https://iids.sdyu.edu.cn/cas/login?service=https://lxl.sdyu.edu.cn/cas/index.php?callback=https://lxl.sdyu.edu.cn/home/web/f_second"
+    )
+
+    driver.find_element(
+        By.XPATH, "/html/body/div/div[2]/div/div/div/div[2]/label/span[2]"
+    ).click()
+    driver.find_element(By.NAME, "username").send_keys(conf["account"]["username"])
+    driver.find_element(By.NAME, "password").send_keys(conf["account"]["password"])
+    driver.find_element(By.NAME, "captcha").click()
+
+    while True:
+        sleep(1)
+        if driver.current_url == "https://lxl.sdyu.edu.cn/home/web/f_second":
+            break
+
+    conf["data"]["PHPSESSID"] = driver.get_cookie("PHPSESSID")["value"]
+    conf["data"]["access_token"] = driver.get_cookie("access_token")["value"]
+    conf["data"]["expire"] = driver.get_cookie("expire")["value"]
+    conf["data"]["user_name"] = driver.get_cookie("user_name")["value"]
+    conf["data"]["userid"] = driver.get_cookie("userid")["value"]
+
+    driver.quit()
+
+    conf["data"]["date"] = datetime.date.today()
+    conf["data"]["segment"] = get_segment(conf["seat"]["seat_area"])
+
+    with open("conf.toml", "wb") as f:
+        tomli_w.dump(conf, f)
+    return conf
+
+
+def wait_12():
+    target_time = datetime.datetime.now().replace(hour=12, minute=0, second=0)
+    while datetime.datetime.now() < target_time:
+        print(f"\r没到点呢:{datetime.datetime.now()}", end="")
+    return
+
+
+def grab_seat(conf):
+    url = f"https://lxl.sdyu.edu.cn/api.php/spaces/{conf['seat']['seat_id']}/book"
+    data = {
+        "access_token": conf["data"]["access_token"],
+        "userid": conf["data"]["userid"],
+        "segment": conf["data"]["segment"],
+        "type": "1",
+        "operateChannel": "2",
+    }
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    }
+    cookies = dict(
+        PHPSESSID=conf["data"]["PHPSESSID"],
+        access_token=conf["data"]["access_token"],
+        expire=conf["data"]["expire"],
+        user_name=conf["data"]["user_name"],
+        userid=conf["data"]["userid"],
+    )
+    retry_times = 10
+    for _ in range(retry_times):
+        r = requests.post(
+            url=url,
+            data=data,
+            headers=headers,
+            cookies=cookies,
+        )
+        print(r.json()["msg"])
+
+        if r.json()["status"] == 0:
+            if (
+                r.json()["msg"] == "参数错误"
+                or r.json()["msg"] == "该空间当前状态不可预约"
+            ):
+                print("来晚了，被约了")
+                break
+            if r.json()["msg"] == "预约超时，请重新预约":
+                print("可能约成功了，重试……")
+                sleep(1)
+            if r.json()["msg"] == "当前用户在该时段已存在预约，不可重复预约":
+                print("你约过别的位了")
+                break
+            if r.json()["msg"] == "由于您长时间未操作，正在重新登录":
+                conf = get_cookies(conf, force=True)
+                grab_seat(conf)
+                break
+        elif r.json()["status"] == 1:
+            break
+
+
+def get_reserved(conf):
+    cookies = dict(
+        PHPSESSID=conf["data"]["PHPSESSID"],
+        access_token=conf["data"]["access_token"],
+        expire=conf["data"]["expire"],
+        user_name=conf["data"]["user_name"],
+        userid=conf["data"]["userid"],
+    )
+    r = requests.get("https://lxl.sdyu.edu.cn/user/index/book", cookies=cookies)
+    for tr in (
+        BeautifulSoup(r.text, "html.parser")
+        .find("table", id="menu_table")
+        .select("tbody tr")
+    ):
+        tds = tr.find_all("td")
+        if "预约成功" in tds[4].text:
+            print(re.sub(r"\s|\t|\n", "", tds[1].text), end="\t\t")
+            print(re.sub(r"\s|\t|\n", "", tds[2].text[0:10]))
+
+
+def main():
+    # 读取配置
+    conf = get_config()
+    if conf["init"]:
+        conf = init_config(conf)
+    print("初始化完成")
+
+    # Cookies
+    conf = get_cookies(conf)
+    print("开始抢座")
+
+    # 计时
+    wait_12()
+
+    # 抢座
+    grab_seat(conf)
+    print()
+
+    # 查看预约状态
+    get_reserved(conf)
+    print("如果没有明天的座位信息，说明抢座失败了")
+
+
+if __name__ == "__main__":
+    main()
+    # print("程序已运行完毕，按任意键继续...")
+    os.system("pause")
